91cce0b8ed939807ae20123f936f5e0e689dd8a3
Matt Williams <mwilliamsnov6@gmail.com>
2017-02-07 15:15:45 -0800

    first commit

---
+++ readme.md
@@ -0,0 +1,30 @@
+```
+  _____ ___  ___ _                                                           
+ |_   _|   \|   (_)                                                          
+   | | | |) | |) |                                                           
+  _|_| |___/|___(_) ___                      ___      __         _           
+ | _ \___ __| |    / __|_ _ ___ ___ _ _     | _ \___ / _|__ _ __| |_ ___ _ _ 
+ |   / -_) _` |_  | (_ | '_/ -_) -_) ' \ _  |   / -_)  _/ _` / _|  _/ _ \ '_|
+ |_|_\___\__,_( )  \___|_| \___\___|_||_( ) |_|_\___|_| \__,_\__|\__\___/_|  
+              |/                        |/                                   
+```
+
+Test-driven development (TDD) is an evolutionary approach to development 
+which combines test-first development where you write a test before you 
+write just enough production code to fulfill that test and refactoring
+
+The steps are simple:
+
+* Write a test
+* Watch the test fail 
+* Write only the code you need to make it pass
+* Refactor that code
+
+Your task is to practice TDD by writing both the functions below and their tests 
+- BUT write the TEST FIRST.
+
+Need an example? Look at any warmup you've completed so far!
+Use TDD to work through the following problems. Write tests to deal with erroneous inputs and edge cases.
+
+[watch a demo](https://vimeo.com/202220880)
+
---
+++ script.js
@@ -0,0 +1,73 @@
+
+//   _____ ___  ___ _                                                           
+//  |_   _|   \|   (_)                                                          
+//    | | | |) | |) |                                                           
+//   _|_| |___/|___(_) ___                      ___      __         _           
+//  | _ \___ __| |    / __|_ _ ___ ___ _ _     | _ \___ / _|__ _ __| |_ ___ _ _ 
+//  |   / -_) _` |_  | (_ | '_/ -_) -_) ' \ _  |   / -_)  _/ _` / _|  _/ _ \ '_|
+//  |_|_\___\__,_( )  \___|_| \___\___|_||_( ) |_|_\___|_| \__,_\__|\__\___/_|  
+//               |/                        |/                                   
+
+// Test-driven development (TDD) is an evolutionary approach to development 
+// which combines test-first development where you write a test before you 
+// write just enough production code to fulfill that test and refactoring
+
+// The steps are simple:
+// * Write a test
+// * Watch the test fail 
+// * Write only the code you need to make it pass
+// * Refactor that code
+
+// Your task is to practice TDD by writing both the functions below and their tests 
+// - BUT write the TEST FIRST.
+
+// Need an example? Look at any warmup you've completed so far!
+// Use TDD to work through the following problems
+// Write tests to deal with erroneous inputs and edge cases
+
+
+//############################################################
+
+// Description:
+
+// Write a function that finds the sum of all its arguments.
+
+// eg:
+
+// sum(1, 2, 3) // => 6
+// sum(8, 2) // => 10
+// sum(1, 2, 3, 4, 5) // => 15
+
+//############################################################
+
+// Description:
+
+// Your task is to make function, which returns the sum of a sequence of integers.
+
+// The sequence is defined by 3 non-negative values: begin, end, step.
+
+// If begin value is greater than the end, function should returns 0
+
+// Examples
+
+// sequenceSum(2,2,2) === 2
+// sequenceSum(2,6,2) === 12 // 2 + 4 + 6
+// sequenceSum(1,5,1) === 15 // 1 + 2 + 3 + 4 + 5
+// sequenceSum(1,5,3) === 5 // 1 + 4
+
+//############################################################
+
+// Description:
+
+// Convert Improper Fraction to Mixed Number
+
+// You will need to convert an improper fraction to a mixed number. For example:
+
+// getMixedNum('18/11'); // Should return '1 7/11'
+// getMixedNum('13/5'); // Should return '2 3/5'
+// getMixedNum('75/10'); // Should return '7 5/10'
+// NOTE: All fractions will be greater than 0.
+
+module.exports = {
+  // What should go here??
+}
